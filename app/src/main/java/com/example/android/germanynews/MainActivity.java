4755570be24de9d646b2f628ab8f0ff135dd27d7
Anas <anas.marrawi@gmail.com>
2017-04-28 14:58:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/germanynews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.germanynews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
